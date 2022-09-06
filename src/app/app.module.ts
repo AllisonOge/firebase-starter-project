bf72d2ab6fe16bd6d74c1049c6d7ecaa662e1298
AllisonOge <44110875+AllisonOge@users.noreply.github.com>
2022-09-07 00:38:39 +0100

    code refactoring i

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation.component';
 import { MainComponent } from './main/main.component';
-import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthComponent } from './auth/auth.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: AuthComponent,
+  },
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, NgbCollapseModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbCollapseModule,
+    RouterModule.forRoot(routes),
+    FontAwesomeModule,
+  ],
   declarations: [AppComponent, NavigationComponent, MainComponent],
   bootstrap: [AppComponent],
 })
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {
+  faGoogle,
+  faGithub,
+  faFacebook,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: ['./auth.component.html'],
-  styles: [``]
+  templateUrl: './auth.component.html',
+  styles: [``],
 })
-
-export class AuthComponent {}
+export class AuthComponent {
+  faGoogle = faGoogle;
+  faGithub = faGithub;
+  faFacebook = faFacebook;
+}
