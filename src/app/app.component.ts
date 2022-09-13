83fb2ab51d622e33e7c2f07e2b24ae2138290220
Allison Ogechukwu <ogechukwukanu@gmail.com>
2022-09-13 17:13:35 +0100

    Mockup changes
    Improve visuals for user authentication and signout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private router: Router, public auth: AuthService) {}
+  authenticate() {
+    this.router.navigate(['/login']);
+  }
+}
