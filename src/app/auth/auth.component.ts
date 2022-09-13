e74ad5041d35981acd786a55b7b369168c7a533d
Allison Ogechukwu <ogechukwukanu@gmail.com>
2022-09-13 17:09:14 +0100

    FirebaseUI initialization and config
    The changes implemented are:
    
    - template mockup with single div
    - definition of firebaseui config for email and google signin auth
    - onloginsuccess implementation for user model in firestore

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui';
+import { EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: ['./auth.component.html'],
-  styles: [``]
+  templateUrl: './auth.component.html',
+  styles: [``],
 })
+export class AuthComponent implements OnInit, OnDestroy {
+  ui: firebaseui.auth.AuthUI;
+  constructor(private afAuth: AngularFireAuth, private auth: AuthService) {}
 
-export class AuthComponent {}
+  ngOnInit() {
+    this.afAuth.app.then((app) => {
+      const uiConfig = {
+        signInOptions: [
+          EmailAuthProvider.PROVIDER_ID,
+          GoogleAuthProvider.PROVIDER_ID,
+        ],
+        callbacks: {
+          signInSuccessWithAuthResult: this.onLogInSuccess.bind(this),
+        },
+      };
+
+      this.ui = new firebaseui.auth.AuthUI(app.auth());
+      this.ui.start('#firebaseui-auth-container', uiConfig);
+    });
+  }
+
+  onLogInSuccess(result) {
+    console.log(result);
+    this.auth.updateUserData(result.user);
+  }
+
+  ngOnDestroy() {
+    this.ui.delete();
+  }
+}
