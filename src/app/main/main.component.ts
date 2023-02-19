82ea8252457556675ff32ffd4e487d7e06894d28
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-02-19 05:14:19 +0000

    view adjustment

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,10 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-main',
   template: `
   <div class="row">
-  <div class="col-sm-12 col-md-3">
+  <div class="col-12 col-lg-3">
     <ng-content select="[sidebar]"></ng-content>
   </div>
-  <div class="col-sm-12 col-md-9">
+  <div class="col-12 col-lg-9">
     <ng-content select="[mainbar]"></ng-content>
   </div>
 </div>
