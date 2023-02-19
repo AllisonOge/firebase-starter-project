5a1749680b43bbcabc162a8684fe893f0b9a8b4c
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-19 08:03:44 +0100

    templating of data component

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,30 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import { Animal } from './../models/animal.model';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: [`
+    .grid-template-column {
+      grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+      grid-gap: 1rem;
+      padding: 10px;
+    }
     .datus {
       width: 100%;
-      max-width: 350px;
+      max-width: 300px;
+    }
+
+    .img-container {
+      height: 150px;
+      background-color: #e0e0e0;
+    }
+
+    .img-container img {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
     }
   `],
 })
 export class DataComponent implements OnInit {
-  // animals$: Animal[] = [{
-  //   name: "North american river otter",
-  //   family: "mammal",
-  //   image: "http://source.unsplash.com/random/1920x1080/?North American river otter",
-  //   endangered: false,
-  //   description: "Other specified injury of right quadriceps muscle, fascia and tendon, subsequent encounter",
-  //   weight: 190
-  // }]
+  animals$: Observable<Animal[]>;
   constructor(public afs: AngularFirestore) {
   }
 
   ngOnInit() {
-    // this.afs.collection<Animal[]>('animals/').snapshotChanges()
+    this.animals$ = this.afs.collection<Animal>("animals").valueChanges()
   }
 }
